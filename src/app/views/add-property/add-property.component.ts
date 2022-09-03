f5da8bbd2d339850bf4f55da2bc49120932db4c4
NitinKumarDev <>
2022-09-03 09:42:34 +0530

    initial commit

---
+++ src/app/views/add-property/add-property.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-property',
+  templateUrl: './add-property.component.html',
+  styleUrls: ['./add-property.component.scss'],
+})
+export class AddPropertyComponent implements OnInit {
+  favoriteColor = '#26ab3c';
+  constructor() {}
+
+  ngOnInit(): void {}
+}
